31e7db4c527f0d09b22a7ae19613701c789bc6f2
DenysVitvitskyi <78040713+DenysVitvitskyi@users.noreply.github.com>
2021-11-05 20:01:49 +0200

    Home Work 3.2.3

--- src/Store/fetch.js
+++ src/Store/fetch.js
@@ -1,4 +1,4 @@
-const key = '22674819-1c6648e1bd9ba2423cbcfb506'
+const key = '22674819-1c6648e1bd9ba2423cbcfb506';
 
 export const searchImg = (input, page) => {
     const response = fetch(`https://pixabay.com/api/?q=${input}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`);
